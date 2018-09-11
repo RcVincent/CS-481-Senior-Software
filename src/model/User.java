a937d0315d6ee0aa9782cae1c9cdb2e7727c1959
Zackeree <zhenry@ycp.edu>
2018-09-11 02:51:47 -0400

    Update User.java
    test commit from home computer, feel free to delete these if this isn't how we plan on going about this

--- src/model/User.java
+++ src/model/User.java
@@ -1,5 +1,46 @@
 package model;
 
 public class User {
-	// This
+	private int UserID;
+	private String Username, Password, Email;
+	
+	/// Constructor(s)
+	
+	public User() {
+		
+	}
+	
+	/// Getters/Setters
+	
+	public void setUserID(int UserID) {
+		this.UserID = UserID;	
+	}
+	
+	public int getUserID() {
+		return this.UserID;	
+	}
+	
+	public void setUsername(String Username) {
+		this.Username = Username;	
+	}
+	
+	public String getUsername() {
+		return this.Username;	
+	}
+	
+	public void setPassword(String Password) {
+		this.Password = Password;	
+	}
+	
+	public String getPassword() {
+		return this.Password;	
+	}
+	
+	public void setEmail(String Email) {
+		this.Email = Email;	
+	}
+	
+	public String getEmail() {
+		return this.Email;	
+	}
 }
