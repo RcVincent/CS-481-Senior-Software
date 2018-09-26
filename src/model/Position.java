04c46e956ef7533c2c904d955e77ee626d19208d
Zackeree <Zackeree@10.0.0.64>
2018-09-26 10:35:32 -0400

    Added methods to Position

--- src/model/Position.java
+++ src/model/Position.java
@@ -3,11 +3,42 @@ package model;
 public class Position {
 	private String Title;
 	private int ID, Priority;
-	// SOP Requirements
+	private SOP Requirements;
 	
 	public Position() {
 		
 	}
 	
 	// Getters/Setters
+	public void setTitle(String Title) {
+		this.Title = Title;	
+	}
+	
+	public String getTitle() {
+		return this.Title;	
+	}
+	
+	public void setID(int ID) {
+		this.ID = ID;	
+	}
+	
+	public int getID() {
+		return this.ID;	
+	}
+	
+	public void setPriority(int Priority) {
+		this.Priority = Priority;	
+	}
+	
+	public int getPriority() {
+		return this.Priority;	
+	}
+	
+	public void setRequirements(SOP Requirements) {
+		this.Requirements = Requirements;	
+	}
+	
+	public SOP getRequirements() {
+		return this.Requirements;	
+	}
 }
