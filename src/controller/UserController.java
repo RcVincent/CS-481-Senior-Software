b141891f9b46bb4e18ab9b290c3d06132c25a257
D e v i n <dglispy@ycp.edu>
2018-09-27 17:12:47 -0400

    Assert Dominance. Take Control!
    I made some controllers. SOPController and PositionController are empty but UserController is almost done.

---
+++ src/controller/UserController.java
@@ -0,0 +1,30 @@
+package controller;
+import model.*;
+public class UserController {
+    
+    U = this.User();
+
+    public void login(){
+        U.setLoginStatus(true);
+    }
+    
+    public void logout(){
+    U.setLoginStatus(false);
+    }
+    
+    public boolean Authenticate(){
+        return false;
+    }
+    
+    public void addPosition(Position p){
+        
+    }
+
+    public void ChangeEmail(){
+
+    }
+    
+    public void Archive(){
+        
+    } 
+}
