7351b31f8d8b33531d738458fe6802a8fb52c7aa
Ryan <rvincent@ycp.edu>
2018-09-28 18:56:22 -0400

    Added the skeletons for the derby DB implementation. tables are next

---
+++ src/DBpersist/DerbyDatabase.java
@@ -0,0 +1,98 @@
+package DBpersist;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+import DBpersist.IDatabase;
+import DBpersist.DBUtil;
+import DBpersist.PersistenceException;
+import DBpersist.DerbyDatabase.Transaction;
+
+
+public class DerbyDatabase implements IDatabase {
+
+	static {
+		try {
+			Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+		} catch (Exception e) {
+			throw new IllegalStateException("Could not load Derby driver");
+		}
+	}
+
+	private interface Transaction<ResultType> {
+		public ResultType execute(Connection conn) throws SQLException;
+	}
+
+	private static final int MAX_ATTEMPTS = 100;
+	
+	//**********************************************************************************************
+	//**********************************************************************************************
+	//methods/querries 
+	
+	
+	
+	
+	//**********************************************************************************************
+	//**********************************************************************************************
+	
+	public<ResultType> ResultType executeTransaction(Transaction<ResultType> txn) {
+		try {
+			return doExecuteTransaction(txn);
+		} catch (SQLException e) {
+			throw new PersistenceException("Transaction failed", e);
+		}
+	}
+
+	public<ResultType> ResultType doExecuteTransaction(Transaction<ResultType> txn) throws SQLException {
+		Connection conn = connect();
+
+		try {
+			int numAttempts = 0;
+			boolean success = false;
+			ResultType result = null;
+
+			while (!success && numAttempts < MAX_ATTEMPTS) {
+				try {
+					result = txn.execute(conn);
+					conn.commit();
+					success = true;
+				} catch (SQLException e) {
+					if (e.getSQLState() != null && e.getSQLState().equals("41000")) {
+						// Deadlock: retry (unless max retry count has been reached)
+						numAttempts++;
+					} else {
+						// Some other kind of SQLException
+						throw e;
+					}
+				}
+			}
+
+			if (!success) {
+				throw new SQLException("Transaction failed (too many retries)");
+			}
+
+			// Success!
+			return result;
+		} finally {
+			DBUtil.closeQuietly(conn);
+		}
+	}
+
+	private Connection connect() throws SQLException {
+		Connection conn = DriverManager.getConnection("jdbc:derby:test.db;create=true");
+
+		// Set autocommit to false to allow multiple the execution of
+		// multiple queries/statements as part of the same transaction.
+		conn.setAutoCommit(false);
+
+		return conn;
+	}
+
+}
--- src/model/User.java
+++ src/model/User.java
@@ -85,4 +85,5 @@ public class User {
 	public void setLoginStatus(boolean l) {
 		loggedin = l;
 	}
+
 }
