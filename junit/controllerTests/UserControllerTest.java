64155c06aeabab94479185bee810bd09ba93fe61
Zackeree <zhenry@ycp.edu>
2018-10-08 17:32:02 -0400

    Started ValidateEmail

--- junit/controllerTests/UserControllerTest.java
+++ junit/controllerTests/UserControllerTest.java
@@ -1,6 +1,29 @@
 package controllerTests;
 
 import org.junit.*;
-public class UserControllerTest {
+import model.User;
+import controller.UserController;
 
+public class UserControllerTest {
+	private User u1, u2, u3;
+	private UserController controller;
+	
+	@Before
+	public void setUp(){
+		u1 = new User();
+		u2 = new User();
+		u3 = new User();
+		
+		u1.setEmail("CEO@Google.com");
+		u2.setEmail("BadEmail@@ycp.edu");
+		u3.setEmail("email");
+		
+		
+		controller = new UserController();
+	}
+	
+	@Test
+	public void testValidateEmail() {
+		// TODO:assertEquals(false, controller.validateEmail(u1.getEmail()));
+	}
 }
