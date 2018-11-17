8ed10866a20b019e7f449af67f9aa9d310da8b07
Ryan <rvincent@ycp.edu>
2018-11-17 16:26:10 -0500

    Filled out the post portion of the system sniffer servlet

--- src/edu/ycp/cs481/servlets/SystemSnifferServlet.java
+++ src/edu/ycp/cs481/servlets/SystemSnifferServlet.java
@@ -11,6 +11,9 @@ import javax.servlet.http.HttpSession;
 import edu.ycp.cs481.control.SystemSnifferController;
 import edu.ycp.cs481.control.UserController;
 import edu.ycp.cs481.model.EnumPermission;
+import edu.ycp.cs481.model.Messenger;
+import edu.ycp.cs481.model.Position;
+import edu.ycp.cs481.model.User;
 
 @SuppressWarnings("serial")
 public class SystemSnifferServlet extends HttpServlet{
@@ -35,6 +38,39 @@ public class SystemSnifferServlet extends HttpServlet{
 	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+		SystemSnifferController sfc = new SystemSnifferController(); 
+		UserController uc = new UserController(); 
+		String action = req.getParameter("WhatToSniff");
 		
+		if(action.equalsIgnoreCase("searchSpecific")) {
+		String searchID = req.getParameter("user_id");
+		
+		if(searchID.equalsIgnoreCase("") || searchID == null) {
+			System.out.println("There is no ID to search by don't do the search.");
+		}
+		else {
+			int search_id = Integer.parseInt(searchID);
+			User u = uc.searchForUsers(search_id, 0, false, "", false, "", false, "", 0, -1).get(0);
+			Position p = u.getPosition();
+			
+			if(!sfc.checkIfToDoIsEmpty(p)) {
+				req.setAttribute("historyNotEmpty", "There are SOPs you must finish");
+				Messenger.main(new String[] {u.getEmail(), "Incomplete Training", u.getFirstName() + ", you have incomplete SOPs in your training"
+						+ "	history, please complete these as soon as possible. A message will also be sent to your manager. Have a great day."});
+			} else {
+				System.out.println("There were no gaps in this users history");
+				req.setAttribute("SuccessMessage", "There were no gaps in this users training history.");
+				
+			}
+			req.getRequestDispatcher("/search_system.jsp").forward(req, resp);
+			
+			
+		}
+		
+		}
+		else if(action.equalsIgnoreCase("searchAll")) {
+			sfc.SniffDeeply();
+			req.getRequestDispatcher("/search_system.jsp").forward(req, resp);
+		}
 	}
 }
