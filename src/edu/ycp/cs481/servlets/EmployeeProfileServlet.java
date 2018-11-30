4a851e8bbf9a8a1fe3dc0de97c72b5fdf6bd1442
Ryan <rvincent@ycp.edu>
2018-11-29 20:47:20 -0500

    adding the employee profile servlet and
    jsp. this is where manager info will be dispalyed
    
    and sops to do will be displayed and accessed

---
+++ src/edu/ycp/cs481/servlets/EmployeeProfileServlet.java
@@ -0,0 +1,25 @@
+package edu.ycp.cs481.servlets;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import edu.ycp.cs481.control.UserController;
+
+@SuppressWarnings("serial")
+public class EmployeeProfileServlet extends HttpServlet{
+
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+		
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+		
+	}
+
+}
