2fac18ea66344c6694aae3bd25be3c3a2ac088d8
Ryan <rvincent@ycp.edu>
2018-11-29 21:46:14 -0500

    filled out the employee profile jsp and got most of the logic done for
    the servlet

--- src/edu/ycp/cs481/servlets/EmployeeProfileServlet.java
+++ src/edu/ycp/cs481/servlets/EmployeeProfileServlet.java
@@ -1,5 +1,7 @@
 package edu.ycp.cs481.servlets;
 import java.io.IOException;
+import java.util.ArrayList;
+
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -8,17 +10,63 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import edu.ycp.cs481.control.UserController;
+import edu.ycp.cs481.model.User;
+import edu.ycp.cs481.model.SOP;
+import edu.ycp.cs481.model.Position;
+
 
 @SuppressWarnings("serial")
 public class EmployeeProfileServlet extends HttpServlet{
 
 	@Override
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
-		
+		HttpSession session = req.getSession();
+		if(session.getAttribute("user_id") == null){
+			resp.sendRedirect(req.getContextPath() + "/login");
+		}else{
+			req.getRequestDispatcher("/employee_profile.jsp").forward(req, resp);
+		}
 	}
 	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+		HttpSession session = req.getSession(); 
+		UserController uc = new UserController(); 
+		int userID = (int) session.getAttribute("user_id");
+		
+		User you = uc.searchForUsers(userID, -1, false, "", false, "", false, "", -1, -1).get(0);
+		Position P = you.getPosition();
+		
+		//change the display size of required SOPs
+		String changePage = req.getParameter("changePage");
+		String changeDisplaySize = req.getParameter("changeDisplaySize");
+		int currentDisplaySize = Integer.parseInt(req.getParameter("displaySize"));
+		
+		if(changePage != null && !changePage.equalsIgnoreCase("")){
+			int currentPage = Integer.parseInt(req.getParameter("page"));
+			if(changePage.equalsIgnoreCase("prev")){
+				req.setAttribute("page", currentPage - 1);
+			}else if(changePage.equalsIgnoreCase("next")){
+				req.setAttribute("page", currentPage + 1);
+			}
+		}else{
+			req.setAttribute("page", 0);
+		}
+		
+		if(changeDisplaySize != null && !changeDisplaySize.equalsIgnoreCase("")){
+			req.setAttribute("displaySize", Integer.parseInt(changeDisplaySize));
+		}else{
+			req.setAttribute("displaySize", currentDisplaySize);
+		}
+		
+		
+		//set information for display in the jsp
+		req.setAttribute("firstName" you.getFirstName());
+		req.setAttribute("lastName", you.getLastName());
+		req.setAttribute("PositionTitle", P.getTitle());
+		
+		ArrayList<SOP> sops = P.getIncompleteSOPs(P);
+		req.setAttribute("sops", sops); 
 		
 	}
 
